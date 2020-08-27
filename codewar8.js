c494a702eb527ba1fe8656124b760e77855a5cfa
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-08-27 11:27:19 -0500

    counts bouncing times for a ball

---
+++ codewar8.js
@@ -0,0 +1,16 @@
+function bouncingBall(h,  bounce,  window) {
+  // your code here
+  let count = 0;
+  let bouncingHeight = h;
+  if (h<=0 || bounce <= 0 || bounce >= 1 || window >= h) {
+    return -1;
+  }
+  for (let i=0; i<h;i++) {
+    bouncingHeight = bouncingHeight*bounce;
+    console.log(bouncingHeight)
+    if (bouncingHeight > window) {
+         count++;
+    }
+  } 
+  return ((count*2)+1);
+}
