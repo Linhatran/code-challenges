4ccb8ed7c298bf1d075e25a597f1e8117aa6eebd
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-08-28 16:46:32 -0500

    converts to military time

--- hackerrank15.js
+++ hackerrank15.js
@@ -3,9 +3,18 @@ function timeConverter(s) {
   let hh = newS.shift() 
  
   if (s.includes('P')) {
+    if (hh === '12') {hh = '12'}
+      else if (hh !== '12') {
     hh = (Number(hh) + 12).toString();
+    }
+    newS.unshift(hh)
+    return newS.join(':').substr(0,8)
+  } 
+  else if (s.includes('A')) {
+    if (hh === '12') {hh = '00'}
     newS.unshift(hh)
     return newS.join(':').substr(0,8)
   }
 }
-console.log(timeConverter('07:05:45PM'))
+console.log(timeConverter('12:05:45AM')) //00:05:45
+console.log(timeConverter('12:45:54PM')) //12:45:54
