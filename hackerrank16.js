a2007405e2a959d86c5651ebe081aece9d75632e
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-08-30 00:06:32 -0500

    rounds students' grades

--- hackerrank16.js
+++ hackerrank16.js
@@ -1,5 +1,17 @@
 function gradingStudents(grades) {
-    for (let i = 0; i < grades.length; i++) {
-     if (grades[i] < 38) {return 'failed'} 
+    // Write your code here
+      let result = [];
+  for (let i = 0; i <grades.length; i++) {
+    if (grades[i] < 38 || (grades[i] % 5 === 0) || (grades[i] >= 38 && (grades[i] % 5 === 1 || grades[i] % 5 === 2))) { 
+      result.push(grades[i]);
     }
+    if (grades[i] >= 38 && grades[i] % 5 === 3) { 
+      result.push(grades[i]+2);
+    }
+    if (grades[i] >= 38 && grades[i] % 5 === 4) { 
+      result.push(grades[i]+1);
+    }
+    
+  } 
+  return result;
 }
