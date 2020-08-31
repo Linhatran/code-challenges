2011244567ba1f585035d8519cc91b7ee52950b6
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-08-31 16:53:05 -0500

    created a person object that introduces themselves

--- top4.js
+++ top4.js
@@ -8,21 +8,23 @@ function Person(first, last, age, gender, interests) {
   this.interests = interests,
   this.bio = function bio() {
     let pronounce = (this.gender === 'male') ? "He":"She";
-    let things;
+    let things = '';
+    
     if (this.interests.length === 1) {
       return things += this.interests[0] + '.';
     } else if (this.interests.length === 2) {
       return things += `${this.interests[0]} and ${this.interests[1]}.` 
     } else {
       for (let i = 0; i < this.interests.length; i++) {
-       if (i === this.interests.length - 1) { 
+       if (i === (this.interests.length - 1)) { 
           things += `and ${this.interests[i]}.`
        } else {
-         return things += `${this.interests[i]}, `}
+          things += `${this.interests[i]}, `}
        }
     }
+    console.log(things)
     console.log(`${this.name.first} ${this.name.last} is ${this.age} years old. ${pronounce} likes ${things}`)
   }
 }
-const person1= new Person('Linh', 'Tran', 23, 'female', ['music', 'skiing', 'kickboxing'])
+const person1= new Person('Linh', 'Tran', 23, 'female', ['music', 'skiing', 'photography'])
 person1.bio()
