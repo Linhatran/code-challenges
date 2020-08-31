aba8dbb2ad06423fbd56ca037c0b780c95b4edc2
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-08-31 16:25:14 -0500

    still need to fix 'undefined' at interests[0]

---
+++ top4.js
@@ -0,0 +1,28 @@
+function Person(first, last, age, gender, interests) {
+  this.name = {
+    first,
+    last,
+  }
+  this.age = age,
+  this.gender = gender,
+  this.interests = interests,
+  this.bio = function bio() {
+    let pronounce = (this.gender === 'male') ? "He":"She";
+    let things;
+    if (this.interests.length === 1) {
+      return things += this.interests[0] + '.';
+    } else if (this.interests.length === 2) {
+      return things += `${this.interests[0]} and ${this.interests[1]}.` 
+    } else {
+      for (let i = 0; i < this.interests.length; i++) {
+       if (i === this.interests.length - 1) { 
+          things += `and ${this.interests[i]}.`
+       } else {
+         return things += `${this.interests[i]}, `}
+       }
+    }
+    console.log(`${this.name.first} ${this.name.last} is ${this.age} years old. ${pronounce} likes ${things}`)
+  }
+}
+const person1= new Person('Linh', 'Tran', 23, 'female', ['music', 'skiing', 'kickboxing'])
+person1.bio()
