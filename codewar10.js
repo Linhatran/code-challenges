6b9f295d545be76a4e4c989c493e4db42e7280c7
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-02 16:27:02 -0500

    updated number functions

--- codewar10.js
+++ codewar10.js
@@ -1,8 +1,37 @@
-function seven() {return 7}
-
-function multiply (x) {
-    function five () {return 5}
-    function times(a,b) {return a*b}
-    return times(five(),x)
+function result(number, operation) {
+  if(!operation) return number;
+      return operation(number);
 }
-console.log(multiply(seven()))
+function zero(operation) {return result(0, operation);}
+function one(operation) {return result(1, operation);}
+function two(operation) {return result(2, operation);}
+function three(operation) {return result(3, operation);}
+function four(operation) {return result(4, operation);}
+function five(operation) {return result(5, operation);}
+function six(operation) {return result(6, operation);}
+function seven(operation) {return result(7, operation);}
+function eight(operation) {return result(8, operation);}
+function nine(operation) {return result(9, operation);}
+
+function times(a) {
+    return function(b) {
+      return b * a;
+    }
+}
+function plus(a) {
+  return function(b) {
+    return b + a;
+  }
+}
+function minus(a) {
+  return function(b) {
+    return b - a;
+  }
+}
+function dividedBy(a) {
+  return function(b) {
+    return Math.floor(b / a);
+  }
+}
+
+console.log(four(times(two())))
