e7b101617f1ed7d69748e89da90d6ad0e40da159
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-03 14:30:47 -0500

    for...in loops

--- csx4.js
+++ csx4.js
@@ -12,3 +12,18 @@ for (let num in checkObj) {
   break;
 }
 console.log(found)
+
+
+
+const checkObj = {
+  oddNum: 1,
+  evenNum: 2,
+  foundNum: 5,
+  randomNum: 18
+};
+
+const objToArray = [];
+// ADD CODE HERE
+for (let prop in checkObj) {
+ if (checkObj[prop] >= 2) (objToArray.push(checkObj[prop]))
+}
