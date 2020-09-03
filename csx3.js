7742d06c8738fac3b5ddb7e63554e6d4bb36607b
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-03 10:37:59 -0500

    while loops

--- csx3.js
+++ csx3.js
@@ -6,3 +6,15 @@ while (count < 8) {
 
 // Uncomment these to check your work!
 console.log(count); 
+
+
+// ADD CODE HERE
+let addThis = 0;
+let sum =0; 
+while (addThis < 10) {
+  sum += addThis;
+  addThis++;
+}
+
+// Uncomment the line below to check your work!
+ console.log(sum);
