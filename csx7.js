239c151cee8dc4dabca5d1e012c42116ac028230
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-03 16:46:30 -0500

    works with objects

--- csx7.js
+++ csx7.js
@@ -7,3 +7,18 @@ for (let i = 0; i <= possibleIterable.length; i++) {
   }
 }
 console.log(divByThree)
+
+
+const sumMe = {
+  hello: 'there',
+  you: 8,
+  are: 7,
+  almost: '10',
+  done: '!'
+};
+let total = 0;
+// ADD CODE HERE
+for (let key in sumMe) {
+  if (typeof sumMe[key] === 'number') {total+= sumMe[key]}
+}
+console.log(total)
