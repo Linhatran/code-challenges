8fa286b0b3f4b91e62426dea68551e90eb906382
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-04 17:58:34 -0500

    invokes functions to console.log a chain of names

---
+++ csx8.js
@@ -0,0 +1,28 @@
+let calls = "";
+
+function jerry(str) {
+	//concat str 
+  str +='Jerry'
+  //return result of kramer
+  return kramer(str)
+}
+
+function george(str) {
+	str+='George';
+  return elaine(str)
+}
+
+function elaine(str) {
+	str+= 'Elaine';
+  return str
+}
+
+function kramer(str) {
+	str+='Kramer';
+  return george(str)
+}
+
+
+// Uncomment these to check your work!
+calls = jerry(calls);
+console.log(calls); // should log: 'JerryKramerGeorgeElaine'
