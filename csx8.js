b116ff8bd23f1cfb0a4305655156982276f5959f
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-04 18:12:13 -0500

    functions invocation

--- csx8.js
+++ csx8.js
@@ -26,3 +26,25 @@ function kramer(str) {
 // Uncomment these to check your work!
 calls = jerry(calls);
 console.log(calls); // should log: 'JerryKramerGeorgeElaine'
+
+
+let friendsAvailable = true;
+
+function makePlans(name) {
+  // INSERT CODE HERE
+  return callFriend(friendsAvailable,name)
+}
+
+function callFriend(bool, name) {
+  // INSERT CODE HERE
+  if (bool) {
+    return `Plans made with ${name} this weekend`
+  } else {
+    return `Everyone is busy this weekend`
+  }
+}
+
+// Uncomment these to check your work!
+console.log(makePlans("Mary")) // should return: "Plans made with Mary this weekend'
+friendsAvailable = false;
+console.log(makePlans("James")) //should return: "Everyone is busy this weekend."
