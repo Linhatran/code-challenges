b22c1c3ec4a9795e4e7f5ad6f71eef694084482e
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-05 08:47:14 -0500

    builds HOFs

--- csx12.js
+++ csx12.js
@@ -38,3 +38,20 @@ console.log(typeof forEach); // should log: 'function'
 forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
 console.log(typeof map); // should log: 'function'
 console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
+
+
+
+function eitherFilter(array, callback1, callback2) {
+  // ADD CODE HERE
+  let newArray = []
+  array.forEach(num => {
+    if (callback1(num) || callback2(num)) newArray.push(num)
+  })
+  return newArray
+}
+
+// Uncomment these to check your work!
+const arrOfNums = [10, 35, 105, 9];
+const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
+const over100 = n => n > 100;
+console.log(eitherFilter(arrOfNums, integerSquareRoot, over100)); // should log: [105, 9]
