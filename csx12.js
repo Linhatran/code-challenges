c288b89c24cab1bb021e473575cbc509e8e7d7cf
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-05 08:37:41 -0500

    builds HOFs

--- csx12.js
+++ csx12.js
@@ -20,3 +20,21 @@ const map = (arr, callback) => {
 console.log(typeof subtractTwo); // should log: 'function'
 console.log(typeof map); // should log: 'function'
 console.log(map([3,4,5], subtractTwo)); // should log: [ 1, 2, 3 ]
+
+
+// ADD CODE HERE
+const forEach = (arr, cb) => {
+  for (let i = 0; i<arr.length; i++) {
+    cb(arr[i])
+  }
+}
+const map = (arr, cb) => {
+  let newArr = [];
+  arr.forEach(num => newArr.push(cb(num)) )
+  return newArr
+}
+// Uncomment these to check your work!
+console.log(typeof forEach); // should log: 'function'
+forEach(['a','b','c'], i => console.log(i)); // should log: 'a', 'b', 'c'
+console.log(typeof map); // should log: 'function'
+console.log(map([3,4,5], n => n - 2)); // should log: [1, 2, 3]
