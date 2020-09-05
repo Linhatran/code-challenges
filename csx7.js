31cfca154e4e8d01bfd433644323132166ef9e5f
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-04 21:59:24 -0500

    works with objects

--- csx7.js
+++ csx7.js
@@ -22,3 +22,13 @@ for (let key in sumMe) {
   if (typeof sumMe[key] === 'number') {total+= sumMe[key]}
 }
 console.log(total)
+
+
+// ADD CODE HERE 
+function addWaldo(obj) {
+  obj['Waldo'] = 'unknown';
+  return obj;
+}
+// Uncomment these to check your work!
+const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
+console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
