98b1fdafe75bf88d307c0975b4adbea95a762db0
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-04 22:01:56 -0500

    works with objects

--- csx7.js
+++ csx7.js
@@ -32,3 +32,13 @@ function addWaldo(obj) {
 // Uncomment these to check your work!
 const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
 console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
+
+// ADD CODE HERE
+function findWaldo(obj) {
+  return (obj['Waldo']) ? obj.Waldo : (`Where's Waldo?`)
+}
+// Uncomment these to check your work!
+const DC = {'Bruce': 'Wayne', 'Harley': 'Quinn'}
+const supernatural = {'Sam': 'Winchester', 'Dean': 'Winchester', 'Waldo': 'unknown'}
+console.log(findWaldo(DC)) // should log: 'Where's Waldo?'
+console.log(findWaldo(supernatural)) // should log: 'unknown'
