5bd7bfc703ca44746ceb90d9ea6eab4f09f788de
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-07 17:02:59 -0500

    scope and closure

--- csx24.js
+++ csx24.js
@@ -9,3 +9,26 @@ const sampleFunc = createFunctionWithInput('sample');
 console.log(sampleFunc()); // should log: 'sample'
 const helloFunc = createFunctionWithInput('hello');
 console.log(helloFunc()); // should log: 'hello'
+ <------------------------------------------------>
+
+function outer() {
+  let counter = 0; // this variable is outside incrementCounter's scope
+  function incrementCounter() {
+    counter++;
+    console.log('counter', counter);
+  }
+  return incrementCounter;
+}
+
+const willCounter = outer();
+const jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+willCounter(); //counter 1
+willCounter(); //counter 2
+willCounter(); //counter 3
+
+jasCounter(); //counter 1
+willCounter(); //counter 4
