35acdd8997861ee9952d3637a5c9c08ee3dc2e36
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-07 17:00:39 -0500

    scope, closure

---
+++ csx24.js
@@ -0,0 +1,11 @@
+// ADD CODE HERE
+function createFunctionWithInput(string) {
+  return function printInput() {
+    return string;
+  }
+}
+// UNCOMMENT THESE TO TEST YOUR WORK!
+const sampleFunc = createFunctionWithInput('sample');
+console.log(sampleFunc()); // should log: 'sample'
+const helloFunc = createFunctionWithInput('hello');
+console.log(helloFunc()); // should log: 'hello'
