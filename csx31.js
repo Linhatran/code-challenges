9fac938b412e3096abe62aab50724027bfbb56af
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-07 19:51:59 -0500

    creates obj with date and output keys

---
+++ csx31.js
@@ -0,0 +1,13 @@
+// ADD CODE HERE
+function dateStamp(func) {
+  let obj = {};
+  return function (arg) {
+    obj.date = ( new Date() ).toString().split(' ').splice(0,4).join(' ')
+    obj.output = func(arg);
+    return obj
+  }
+}
+// Uncomment these to check your work!
+const stampedMultBy2 = dateStamp(n => n * 2);
+console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
