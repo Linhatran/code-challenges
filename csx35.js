afe71c0551cb9e7ee1b2fdd5fa2b8cb4fbdb6154
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-10 19:28:51 -0500

    getLength()

--- csx35.js
+++ csx35.js
@@ -10,3 +10,16 @@ console.log(getLength([1])); // -> 1
 console.log(getLength([1, 2])); // -> 2
 console.log(getLength([1, 2, 3, 4, 5])); // -> 5
 console.log(getLength([])); // -> 0
+
+	///// second method
+function getLength(array, length = 0) {
+  	if (array[0] === undefined) return length;
+    length++;
+  	return getLength(array.slice(1), length);
+}
+
+// To check if you've completed the challenge, uncomment these console.logs!
+console.log(getLength([1])); // -> 1
+console.log(getLength([1, 2])); // -> 2
+console.log(getLength([1, 2, 3, 4, 5])); // -> 5
+console.log(getLength([])); // -> 0
