72444c838cadf6e099315636a737e6fa35c26585
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-22 21:17:51 -0500

    test for assertEqual with every()

---
+++ hackreactor7.js
@@ -0,0 +1,33 @@
+// Note: This is a simple, albeit temporarily incorrect implementation of the standard Array method "every()":
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+
+// FUNCTION DEFINITION(S)
+function every(array, callbackFunction) {
+  var doesEveryElementMatch = true;
+
+  for (var i = 0; i < array.length; i++) {
+    doesEveryElementMatch = callbackFunction(array[i]);
+  }
+
+  return doesEveryElementMatch;
+}
+
+function isGreaterThanFive(...array) {
+    for (let value of array) {
+    return (value > 5) ? true : false
+    }
+}
+const everyOutcome = every([2,4,7,8,3,124], isGreaterThanFive)
+const result = isGreaterThanFive([2,4,7,8,3,124])
+
+// ASSERTION FUNCTION(S) TO BE USED
+function assertEqual (expect, actual, testName) {
+    if (expect === actual) {
+        console.log('passed')
+    } else {
+        console.log(`FAILED [${testName}] Expected ${expect} but got ${actual}`)
+    }
+}
+
+// TESTS CASES
+assertEqual(result, everyOutcome, 'check if everything is greater than 5')
