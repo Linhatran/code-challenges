6c20a90a7ecfe6dddb5cbe7681047a160d5cee07
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-24 22:09:45 -0500

    countdown()

---
+++ recursion1.js
@@ -0,0 +1,17 @@
+// // Challenge 1
+// function countdown(n) {
+//   //set base case n = 0
+  
+//   if (n === 0) {
+//     return;
+//   }
+//    console.log(n)
+//   return countdown(n-1)
+//   //recursive case: 
+//   //console.log(n)
+//   //return recursive call countdown (n-1)
+// }
+
+// // To check if you've completed it, uncomment these console.logs!
+// //countdown(5);
+// //countdown(10);
