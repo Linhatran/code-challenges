93d42f1f6ca2ad35fb94109180b03fbd1b2a8af1
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-25 14:55:46 -0500

    isPrime() recursive

---
+++ recursion3.js
@@ -0,0 +1,13 @@
+function isPrime(num, i = num-1) {
+  	if (num < 2) return false;
+  	if (i <= 1) return true;
+  	//base case
+  	if (num % i === 0 ) return false;
+    return isPrime(num, i-1)
+}
+
+
+console.log(isPrime(1)); //-> false
+console.log(isPrime(2)); //-> true
+console.log(isPrime(3)); //-> true
+console.log(isPrime(9)); //-> false
