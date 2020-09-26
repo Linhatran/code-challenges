365b9fa19213ce62983ae46521585f9dd8240fe8
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-26 13:34:15 -0500

    getRangeBetween()

--- recursion2.js
+++ recursion2.js
@@ -2,11 +2,14 @@
 //Write a function getRangeBetween that returns an array of all integers between values x and y, not including x and y.
 
 function getRangeBetween(x, y) {
-	//base case x + 1 >= y
-  let array = [x+1]
-  if (x+2 >= y ) {return array}
-  //recursive call
-  return array.concat(getRangeBetween(x+1,y))
+	//base case 
+  	//if x+1 = y, stop, return an array 
+  let result = [];
+  if (x+1 === y) return result;
+  //start at x, log x+1 
+  result.push(x+1)
+  return result.concat(getRangeBetween(x+1, y))
 }
-// console.log(getRangeBetween(2, 9)) //-> [3, 4, 5, 6, 7, 8]
-// console.log(getRangeBetween(-5, 5)) //-> [-4, -3, -2, 1, 0, 1, 2, 3, 4]
+
+console.log(getRangeBetween(2, 9)) //-> [3, 4, 5, 6, 7, 8]
+console.log(getRangeBetween(-5, 5)) //-> [-4, -3, -2, 1, 0, 1, 2, 3, 4]
