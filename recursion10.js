f5a746e8692ab7c0c05f13c8c310c2a4359e560a
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-26 13:57:55 -0500

    findInOrderedSet()

--- recursion10.js
+++ recursion10.js
@@ -4,6 +4,29 @@ function findInOrderedSet(arr, target) {
   if (arr[0] === target) return true;
   return findInOrderedSet(arr.slice(1), target)
 }
+<------------------------------------------------------>
+function findInOrderedSet(arr, target) {
+	let min = 0;
+  let max = arr.length-1;
+  
+  while (min <= max) {
+    let guess = Math.floor((max + min)/2)
+    if (arr[guess] === target) {
+      return true;
+    } else {
+      if (arr[guess] > target) {
+        max = guess - 1;
+      } else {
+        min = guess + 1;
+      }
+    }
+  }
+  return false
+}
+const nums = [1, 4, 6, 7, 9, 17, 45];
+console.log(findInOrderedSet(nums, 9));  //-> true
+console.log(findInOrderedSet(nums, 2));  //-> false
+
 
 const nums = [1, 4, 6, 7, 9, 17, 45];
 console.log(findInOrderedSet(nums, 9));  //-> true
