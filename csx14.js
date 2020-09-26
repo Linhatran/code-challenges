b1e3654581f2c413cebb721a7f82afc92a082e89
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-26 16:15:20 -0500

    intersection()

--- csx14.js
+++ csx14.js
@@ -1,21 +1,7 @@
 // ADD CODE HERE
-function intersection (arrays) {
-    function reduce(arr, callback, initialValue) {
-      	let accumulator = initialValue;
-      	arr.forEach(num => accumulator = callback(accumulator, num))
-      	return accumulator;
-    }
-  	function filter(array1, array2) {
-    		let newArr = []
-    		array1.forEach(num => (array2.includes(num)) ? newArr.push(num) : null)
-    		return newArr
-  	}
-  return reduce(arrays, filter, arrays[0])
-}
-
-function toReduce(array) {
-  return array.reduce((accumulator, item) => (accumulator.includes(item)) ? accumulator : [...accumulator, item],[]) //accumulator first = []
-  //return accumulator 
+function intersection(...arrays) {
+  return arrays.reduce((array1, array2) => array1.filter(num => array2.includes(num)))
+	
 }
 // Uncomment these to check your work!
 const arr1 = [5, 10, 15, 20];
