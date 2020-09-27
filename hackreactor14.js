0c3563bc6d6ae628b057d9d8c4a7ac2c589cf7d5
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-27 11:17:50 -0500

    flipPairs()

---
+++ hackreactor14.js
@@ -0,0 +1,19 @@
+// Flip every pair of characters in a string.
+
+
+// Example:
+// var input = 'check out how interesting this problem is, it\'s insanely interesting!';
+// var output = flipPairs(input);
+// console.log(output); // --> hcce kuo toh wnietertsni ghtsip orlbmei ,si 't sniasenyli tnreseitgn!
+function flipPairs(string) {
+  let output = '';
+  for (let i = 0; i < string.length; i+=2) {
+    if (string[i] === undefined) {
+      output+= string[i];
+    }
+    output+= string[i+1];
+    output+= string[i];
+  }
+  return output;
+};
+console.log(flipPairs('abcd'))
