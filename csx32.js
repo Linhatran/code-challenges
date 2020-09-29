048aa4856c558456976d210c1980d6128c013839
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-09-29 14:25:12 -0500

    censor()

--- csx32.js
+++ csx32.js
@@ -13,6 +13,26 @@ function censor () {
     }
   }
 
+<----------------------------------------->
+  function censor() {
+  let first = []; //quick, dogs
+  let second = []; //slow, cats
+	return function (...strings) {
+    if (strings.length === 2) {
+      first.push(strings[0]);
+      second.push(strings[1]);
+    } else if (strings.length === 1) {
+    	strings = strings.toString();
+      for (let i = 0; i < first.length; i++) {
+        if (strings.includes(first[i])) {
+          strings = strings.replace(`${first[i]}`, `${second[i]}`)    
+        }
+      }
+      return strings;
+    }
+  }
+}
+
 // Uncomment these to check your work!
 const changeScene = censor();
 changeScene('dogs', 'cats');
