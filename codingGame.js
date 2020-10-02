891ade60e315ccd24dcaa05eb0dbc28c0129e67d
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-02 13:50:57 -0500

    Update codingGame.js

--- codingGame.js
+++ codingGame.js
@@ -1,3 +1,4 @@
+//Question 1: Sum all numbers
 function sumRange(num) {
   //set a base case
     //num = 1, return num
@@ -9,3 +10,21 @@ function sumRange(num) {
 }
 console.log(sumRange(3))
 //sumRange(3) returns 6, since 1 + 2 + 3 = 6.
+
+
+//Question 2: Power function
+function power(base, exp) {
+  //set base case
+    //small problem: exp === 0, result: base ** 0 === 1
+  //recursive call 
+    //exp ++
+    //each time exp - 1, base * base
+    //return base * power(exp-1)
+  if (exp === 0) return base ** 0;
+  return base* power(base, exp - 1)
+}
+console.log(power(2, 4)); // 16
+console.log(power(2, 3)); // 8
+console.log(power(2, 2)); // 4 
+console.log(power(2, 1)); // 2
+console.log(power(2, 0)); // 1
