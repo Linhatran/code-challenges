8c721a23abd45502b57880e15b7eefe1aa9f9cfd
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-04 12:07:12 -0500

    Update recursion11.js

--- recursion11.js
+++ recursion11.js
@@ -35,3 +35,28 @@ function totalIntegers(array) {
   return total + totalIntegers(array)
 }
 console.log(totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]])); // 7
+
+//Question 8: sum all squares of each number 
+function SumSquares(array) {
+  if (array.length === 0) return 0;
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      result += SumSquares(array[i])
+    } else {
+      result += array[i]*array[i]
+    }
+  }
+  return result
+}
+var l = [1,2,3]; 
+console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+
+l = [[1,2],3]; 
+console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+
+l = [[[[[[[[[1]]]]]]]]] 
+console.log(SumSquares(l)); // 1 = 1
+
+l = [10,[[10],10],[10]] 
+console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
