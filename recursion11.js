0cc24f7655e829eebc917f9c0423bfe202f54d94
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-04 12:24:20 -0500

    Update recursion11.js

--- recursion11.js
+++ recursion11.js
@@ -60,3 +60,22 @@ console.log(SumSquares(l)); // 1 = 1
 
 l = [10,[[10],10],[10]] 
 console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
+
+//Question 9: The function should return an array containing repetitions of 
+//the number argument. For instance, replicate(3, 5) should return [5,5,5].
+//If the times argument is negative, return an empty array.
+
+function replicate(times, value) {
+  let result = [];
+  //base case 
+    //times <= 0
+    //do nothing: return result;
+  //recursive call
+    //result.push(value)
+    //return replicate(times-1, value)
+  if (times <= 0) return result;
+  return [value].concat(replicate(times-1, value))
+}
+console.log(replicate(3, 5)) // [5, 5, 5]
+console.log(replicate(1, 69)) // [69]
+console.log(replicate(-2, 6)) // []
