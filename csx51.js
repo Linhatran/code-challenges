fb0ea591c31f919b57be95aa3407bb3441fe8763
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-06 11:33:20 -0500

    once()

--- csx51.js
+++ csx51.js
@@ -18,10 +18,3 @@ console.log(addByTwoOnce(5));  //should log 7
 console.log(addByTwoOnce(10));  //should log 7
 console.log(addByTwoOnce(9001));  //should log 7
 
-// /*** Uncomment these to check your work! ***/
-const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
-const getDay = cycleIterator(threeDayWeekend);
-console.log(getDay()); // => should log 'Fri'
-console.log(getDay()); // => should log 'Sat'
-console.log(getDay()); // => should log 'Sun'
-console.log(getDay()); // => should log 'Fri'
