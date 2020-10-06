36acc5d1706e0be1ee300eca9a9c4bb3e5bd6a6d
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-06 11:32:51 -0500

    cycleIterator(array)

--- csx51.js
+++ csx51.js
@@ -1,11 +1,22 @@
-function cycleIterator(array) {
-  let i = 0;
-	return function () { 
-    if (i === array.length) i = 0;
-    console.log(array[i])
-    i++; 
-    }
+
+
+// ADD CODE HERE
+function once(cb) {
+  let counter = 0;
+  let result;
+  return function doStuff(num) {
+    counter++;
+    return (counter <= 1) ? result = cb(num) : result;
+  }
 }
+const addByTwoOnce = once(function(num) {
+  return num + 2;
+});
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+console.log(addByTwoOnce(5));  //should log 7
+console.log(addByTwoOnce(10));  //should log 7
+console.log(addByTwoOnce(9001));  //should log 7
 
 // /*** Uncomment these to check your work! ***/
 const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
