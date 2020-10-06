ff5d897f0ffad1cc1bed31a99ccf093bd811a299
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-06 11:34:35 -0500

    cycleIterator (array)

--- csx51.js
+++ csx51.js
@@ -1,20 +1,19 @@
-
-
-// ADD CODE HERE
-function once(cb) {
-  let counter = 0;
-  let result;
-  return function doStuff(num) {
-    counter++;
-    return (counter <= 1) ? result = cb(num) : result;
+ADD CODE HERE
+function cycleIterator (array) {
+  //declare an empty array
+  //loop over array, push each index into an empty array
+  //with each fn call, return the first element of that empty array
+  let myDays = [];
+  return function getDay() {
+    array.forEach(day => myDays.push(day));
+    return myDays.shift()
   }
 }
-const addByTwoOnce = once(function(num) {
-  return num + 2;
-});
 
-// UNCOMMENT THESE TO TEST YOUR WORK!
-console.log(addByTwoOnce(5));  //should log 7
-console.log(addByTwoOnce(10));  //should log 7
-console.log(addByTwoOnce(9001));  //should log 7
-
+// Uncomment these to check your work!
+const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+const getDay = cycleIterator(threeDayWeekend);
+console.log(getDay()); // should log: 'Fri'
+console.log(getDay()); // should log: 'Sat'
+console.log(getDay()); // should log: 'Sun'
+console.log(getDay()); // should log: 'Fri'
