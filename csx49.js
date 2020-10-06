85732e91e57e56e0c7836270e77950648ba3d35f
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-06 11:20:56 -0500

    Update csx49.js

--- csx49.js
+++ csx49.js
@@ -15,3 +15,22 @@ const onceFunc = once(addByTwo);
 console.log(onceFunc(4));  // => should log 6
 console.log(onceFunc(10));  // => should log 6
 console.log(onceFunc(9001));  // => should log 6
+
+-------------------------------------------
+	// ADD CODE HERE
+function once(cb) {
+  let counter = 0;
+  let result;
+  return function doStuff(num) {
+    counter++;
+    return (counter <= 1) ? result = cb(num) : result;
+  }
+}
+const addByTwoOnce = once(function(num) {
+  return num + 2;
+});
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+console.log(addByTwoOnce(5));  //should log 7
+console.log(addByTwoOnce(10));  //should log 7
+console.log(addByTwoOnce(9001));  //should log 7
