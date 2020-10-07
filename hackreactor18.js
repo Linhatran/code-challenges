ca78433dd515ac0f2917bc81c8b7cc66e3520517
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-07 11:00:52 -0500

    findPairOfSum()

--- hackreactor18.js
+++ hackreactor18.js
@@ -3,7 +3,7 @@ function findPairForSum(array, target) {
     //loop through the remaining 
     //if num from inside loop + num from outside loop = target, return them
   for (let i = 0; i < array.length; i++) {
-    for (let j = 1; j < array.length; j++) {
+    for (let j = i+1; j < array.length; j++) {
       if (array[i] + array[j] === target) {
         return [array[i],array[j]]
       }
