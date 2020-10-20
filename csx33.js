48e7e08d006f692fe742e17b85917f240e53a042
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-20 12:46:54 -0500

    repeater()

--- csx33.js
+++ csx33.js
@@ -7,6 +7,12 @@ function repeater(char) {
   }
   return string;
 }
+function repeater(char, n = 5) {
+	let result = [];
+  if (n <= 0) return result;
+  result.push(char);
+  return result.concat(repeater(char, n-1)).join('')
+}
 
 // To check if you've completed the challenge, uncomment these console.logs!
 console.log(repeater('g'));
