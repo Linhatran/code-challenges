a92230eaf54ba805630eeb5034542c78f3b45ab7
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-10-22 16:13:42 -0500

    isPrime() using Math.sqrt()

--- codewar6.js
+++ codewar6.js
@@ -1,11 +1,12 @@
-function isPrime(num) {
-	if (num < 2) return false;
-  if (num === 2) return true;
-  // num % n === 0; n = num or 1 
- 	for (let i = num-1; i > 1; i--) {
-    if (num % i === 0) {
-      return false
-    }
+function isPrime(n) {
+  let divisor = 2;
+  let limit = Math.sqrt(n);
+  if (n === 2 || n === 3) return true;
+   if (n % 2 === 0) return false;
+  while (divisor <= limit) {
+    if (n % divisor === 0) return false;
+    divisor+=2;
   }
   return true
 }
+console.log(isPrime(13))
