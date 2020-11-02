fb68c9f49c98b6da0bcc9aa819f2f0d586617916
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-11-02 16:04:40 -0600

    findPairForSum(), findLargestSum()

--- hackreactor18.js
+++ hackreactor18.js
@@ -12,3 +12,26 @@ function findPairForSum(array, target) {
 }
 var pair = findPairForSum([3, 34, 4, 12, 5, 2], 38);
 console.log(pair); // --> [4, 5]
+
+
+
+function findLargestSum(arr) {
+  let biggest = arr[0];
+  let second = arr[1];
+  if (arr.length < 2) return biggest;
+
+  if (biggest < second) {
+    biggest = arr[1];
+    second = arr[0];
+  }
+  for (let i = 2; i < arr.length; i++) {
+    if (biggest < arr[i]) {
+      second = biggest;
+      biggest = arr[i];
+    } else if (second < arr[i]) {
+      second = arr[i];
+    }
+  }
+  return biggest + second;
+}
+//console.log(findLargestSum([4, 6, 123, 78, 4, 67, 37, 9])); //201
