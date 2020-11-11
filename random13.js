4254b2f1516b89b41cd55740abcc0151cd36067c
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-11-11 14:59:31 -0600

    makeChange() greedy algorithm and brute force method

--- random13.js
+++ random13.js
@@ -1,3 +1,5 @@
+// greedy algorithm
+
 function makeChange (coins, amount) { //5,10,25
   coins.sort((a,b) => b-a);
   let i = 0; 
@@ -11,5 +13,21 @@ function makeChange (coins, amount) { //5,10,25
     }
   }
   return coinsTotal;
+}
+console.log(makeChange([5,25,10], 50))
+
+//brute force solution with recursion 
+function makeChange (coins, amount) {
+  if (amount === 0) return 0;
+  let minCoins;
+  coins.forEach((coin) => {
+    if (coin <= amount) {
+      let currentMinCoin = makeChange(coins, amount-coin)
+      if (minCoins === undefined || currentMinCoin < minCoins) {
+        minCoins = currentMinCoin;
+      }
+    }
+  })
+  return minCoins + 1;
 }
 console.log(makeChange([5,25,10], 50))
