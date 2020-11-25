4887eba4ff61e3c18094a12a62245f939efaa73d
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-11-25 11:54:53 -0600

    pow()

--- csx36.js
+++ csx36.js
@@ -1,7 +1,7 @@
 function pow(base, exponent) {
 		return (exponent !== 1) ? base*pow(base,exponent-1) : base
 }
-
+//can add cache to memoize value to speed up the calculation like with fib
 // To check if you've completed the challenge, uncomment these console.logs!
 console.log(pow(2, 4)); // -> 16
 console.log(pow(3, 5)); // -> 243
