d3ce7c09048a573f906d37d38133dd65065dc3c9
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-11-27 14:34:43 -0600

    isValid() brackets

--- leetCode3.js
+++ leetCode3.js
@@ -1,17 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 var isValid = function(s) {
     //opening brackets
     const valids = ['()', '[]', '{}'];
-   const openBrackets = '([{';
-   const closingBrackets = ')]}';
-  const stack = [];
-  const length = s.split('').length;
-
-  if (closingBrackets.includes(s[0])) return false;
+    const openBrackets = {
+        "(" : true,
+        "{" : true,
+        "[" : true
+    }
+    const closingBrackets = {
+        ")" : true,
+        "}" : true,
+        "]" : true
+    }
+    const stack = [];
+    const length = s.split('').length;
+    if (length <= 1) return false;
   for (let i = 0; i < length; i++) {
-    if (openBrackets.includes(s[i])) {
+    if (openBrackets[s[i]]) {
       stack.push(s[i]);
     } 
-    else if (closingBrackets.includes(s[i])) {
+    else if (closingBrackets[s[i]]) {
       const leftBracket = stack.pop();
       const pair = leftBracket+s[i];
       if (!valids.includes(pair)) {
@@ -19,5 +30,5 @@ var isValid = function(s) {
       }
     }
   }
-    return true;
+    return (!stack.length);
 };
