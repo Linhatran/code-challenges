0acf712dd99ae916d146026322962417f789b51e
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-12-05 23:49:00 -0600

    maxDepth() nested array depth

---
+++ hackhour8.js
@@ -0,0 +1,12 @@
+const maxDepth = (arr) => {
+  //declare a local depth var 
+  let count = 0;
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      count += Math.max(maxDepth(item), count)
+    } 
+  }
+  //return local depth plus 1 to account for current array
+  return count + 1;
+}
+console.log(maxDepth([3,[4,[6],[7,0]]]))
