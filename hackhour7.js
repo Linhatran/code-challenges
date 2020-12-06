e254270125a42995cd705140eb28f9eaf832729e
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-12-05 23:21:01 -0600

    sumNestedArray()

---
+++ hackhour7.js
@@ -0,0 +1,12 @@
+const sumNested = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      sum += sumNested(arr[i])
+    } else {
+      sum += arr[i]
+    }
+  }
+  return sum;
+}
+console.log(sumNested([3,[4,[6],[7,0]]]))
