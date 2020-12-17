a37c18bfb22a2936a2c1ddf083b1681817520bc0
Linhatran <41355392+Linhatran@users.noreply.github.com>
2020-12-17 11:47:42 -0600

    memoize()

--- random10.js
+++ random10.js
@@ -13,3 +13,16 @@ function memoize(func) {
 }
 const memoClosureTimes10 = memoize(times10);
 console.log(memoClosureTimes10(5))
+
+
+const cachePromiseFunction2 = (get) => {
+  const cache = {};
+  return function cachedPromiseFunc(url) {
+    if (!cache[url]) {
+      cache[url] = Promise.resolve(get(url));
+      return cache[url];
+    } else {
+      return cache[url];
+    }
+  };
+};
