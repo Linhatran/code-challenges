4d42ede55d72c60859dedbf66ca9a3435e8bfe16
Linhatran <41355392+Linhatran@users.noreply.github.com>
2021-01-12 11:01:09 -0600

    bstMinMax

--- hackhour15.js
+++ hackhour15.js
@@ -16,3 +16,32 @@ const bstMinMax = (root) => {
   //find difference
   return max - min;
 };
+
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two
+given nodes "p" and "q" in the BST. Return the LCA node itself.
+
+Ex:
+
+     4
+   /   \
+  2     7
+ / \     \
+1   3     9
+         /
+        8
+
+The LCA of node 1 and node 3 is node 2.
+The LCA of node 8 and node 9 is node 9.
+The LCA of node 2 and node 8 is node 4.
+
+*/
+
+const lowestCommonAncestor = (root, p, q) => {
+  if (p.value < root.value && q.value < root.value) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (p.value > root.value && q.value > root.value) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+};
