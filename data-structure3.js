175bdf94efa85e303485c7dce1b5458d53e1023f
Linhatran <41355392+Linhatran@users.noreply.github.com>
2021-01-15 16:06:39 -0600

    singly linked lists and doubly linked lists

--- data-structure3.js
+++ data-structure3.js
@@ -105,3 +105,82 @@ list.push(7);
 list.push(6);
 list.push(4);
 console.log(list.contains(9));
+
+/*
+  * Below, you are provided the construtors for a doubly linked list as well as a constructor for the nodes it's composed of
+  * Construct a doubly linked list & modify it's prototype to contain methods named 'add' and 'remove'
+  * The 'add' method should add an additional node to the end of the doubly linked list
+  * The 'remove' method should remove the first instance of a node containing a specific value from the doubly linked list
+  
+  Example doubly linked list
+  null <-> 4 <-> 9 <-> 2 <-> 1 <-> null
+
+  Example after using 'add' method to add 6
+  null <-> 4 <-> 9 <-> 2 <-> 1 <-> 6 <-> null
+
+  Example after using 'remove' method to remove 2
+  null <-> 4 <-> 9 <-> 1 <-> 6 <-> null
+
+  NOTE: must use non-arrow functions for object constructors and prototype methods
+  @see: https://dmitripavlutin.com/when-not-to-use-arrow-functions-in-javascript/
+*/
+
+function DoublyLinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(val) {
+  this.val = val;
+  this.next = null;
+  this.prev = null;
+}
+
+/*
+This method should add a node to the end of the doubly linked list
+ */
+DoublyLinkedList.prototype.add = function (val) {
+  const node = new Node(val);
+  if (!this.head) {
+    this.head = node;
+    this.tail = node;
+  } else {
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+  }
+};
+const list = new DoublyLinkedList();
+
+list.add(3);
+list.add(5);
+list.add(7);
+
+/*
+This method should remove the first instance of a node with the inputted value from the doubly linked list
+ */
+DoublyLinkedList.prototype.remove = function (val) {
+  if (!val) return undefined;
+  let prevNode = this.head;
+  let currentNode = this.head;
+  if (this.head.val === val) {
+    this.head = this.head.next;
+    this.head.prev = null;
+    return;
+  }
+  if (this.tail.val === val) {
+    prevNode = this.tail.prev;
+    prevNode.next = null;
+    this.tail = prevNode;
+    return;
+  }
+  while (currentNode.val !== val) {
+    prevNode = currentNode;
+    currentNode = currentNode.next;
+  }
+  prevNode.next = currentNode.next;
+  currentNode.next.prev = prevNode;
+};
+
+module.exports = { DoublyLinkedList, Node };
+
