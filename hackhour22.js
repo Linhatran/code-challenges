a56496091fe23fd9c8a30e210cf11f1bb4366401
Linhatran <41355392+Linhatran@users.noreply.github.com>
2021-01-28 11:31:49 -0600

    generateParentheses()

--- hackhour22.js
+++ hackhour22.js
@@ -30,19 +30,25 @@ Given n = 0, a solution set is:
 
 const generateParentheses = n => {
   const result = [];
-  const addParens = (n, leftParen = 0, rightParen = 0, string = '') => {
-    if (leftParen === n && rightParen === n) result.push(string);
-    if (leftParen < n) {
-      const str = string + '(';
-      addParens(n, leftParen+1, rightParen, str);
-    }
-    if (rightParen < leftParen) {
-        const str = string + ')';
-      addParens(n, leftParen, rightParen+1, str)
-    }
-    return string;
+  // const addParens = (n, leftParen = 0, rightParen = 0, string = '') => {
+  //   if (leftParen === n && rightParen === n) result.push(string);
+  //   if (leftParen < n) {
+  //     const str = string + '(';
+  //     addParens(n, leftParen+1, rightParen, str);
+  //   }
+  //   if (rightParen < leftParen) {
+  //       const str = string + ')';
+  //     addParens(n, leftParen, rightParen+1, str)
+  //   }
+  //   return string;
+  // }
+  // addParens(n)
+  const generate = (str,l,r) => {
+    if (r === 0) result.push(str);
+    if (l > 0) generate(str + '(', l-1,r);
+    if (r > l) generate(str + ')', l, r-1);
   }
-  addParens(n)
+  generate('', n, n)
   return result;
 };
 console.log(generateParentheses(3))
