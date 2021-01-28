ff7830c08604e5b8c90533c95d0da827a24f212f
Linhatran <41355392+Linhatran@users.noreply.github.com>
2021-01-28 11:27:51 -0600

    generateParentheses.js

---
+++ hackhour22.js
@@ -0,0 +1,49 @@
+/*
+
+Given n pairs of parentheses, write a function to generate all combinations of
+well-formed parentheses.
+
+For example, given n = 2, a solution set is:
+
+[
+  "(())",
+  "()()"
+]
+
+Given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
+Given n = 0, a solution set is:
+
+[
+  ""
+]
+
+*/
+
+const generateParentheses = n => {
+  const result = [];
+  const addParens = (n, leftParen = 0, rightParen = 0, string = '') => {
+    if (leftParen === n && rightParen === n) result.push(string);
+    if (leftParen < n) {
+      const str = string + '(';
+      addParens(n, leftParen+1, rightParen, str);
+    }
+    if (rightParen < leftParen) {
+        const str = string + ')';
+      addParens(n, leftParen, rightParen+1, str)
+    }
+    return string;
+  }
+  addParens(n)
+  return result;
+};
+console.log(generateParentheses(3))
+module.exports = {generateParentheses};
