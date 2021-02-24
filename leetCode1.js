61366d0ba61d4cd588413a3eb20fcc4142de82bc
Linhatran <41355392+Linhatran@users.noreply.github.com>
2021-02-23 22:44:14 -0600

    lengthOfLongestSubstring

--- leetCode1.js
+++ leetCode1.js
@@ -13,6 +13,28 @@ var lengthOfLongestSubstring = function(s) {
     return Math.max(...lengthsOfSubstring)
     
 };
+
+const getLongest = (n) => {
+  const cache = new Map();
+  let start = 0;
+  let max = 0;
+  for (let i = 0; i < n.length; i++) {
+    const curr = n[i];
+    //check if cache has that char and index of cached char is greater than start 
+    if (cache.has(curr) && cache.get(curr) >= start) {
+      //if duplicate is at i, then new (longer) substring would start at the next index
+      start = i+1;
+   
+    }
+    // update index of current char to be the value of curr in cache
+    cache.set(curr, i);
+    // i - start + 1: to find the length of substring
+    max = Math.max(max, i - start + 1)
+  }
+  return max
+}
+console.log(getLongest('OBAMAVZXSDCARE'))
+
 console.log(lengthOfLongestSubstring("pwwkew")) //3
 // console.log(lengthOfLongestSubstring("bbbbbb")) //1
 // console.log(lengthOfLongestSubstring('abcabcbb')) //3
