44f12b513b77510068aaebe0a727c7becc75b566
Linhatran <41355392+Linhatran@users.noreply.github.com>
2021-04-06 15:09:01 -0500

    matrix-transpose and matrix-rotate

--- hackhour12.js
+++ hackhour12.js
@@ -45,3 +45,35 @@ const twoDimArray = [
   [true, false],
 ];
 console.log(matrixTranspose(twoDimArray));
+
+
+Extension:
+Given an nxn matrix, write a function called 'matrixRotate' that rotates the matrix 90 degrees clockwise.
+If given an mxn matrix, return undefined.
+
+For example:  
+const matrix = [  [1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]  ]
+
+console.table(matrixRotate(matrix)) // ->  [  [7, 4, 1],
+                                              [8, 5, 2],
+                                              [9, 6, 3]  ]
+
+BONUS: Rotate the matrix in place. In other words, the space complexity of the solution should be O(1).
+
+*/
+
+const matrixRotate = (matrix) => {
+  if (!matrix.length) return matrix;
+  if (matrix[0].length !== matrix.length) return undefined;
+  const result = []
+  for (let i = 0; i < matrix.length; i++) {
+    const row = [];
+    for (let j = matrix.length-1; j >=0; j--) {
+      row.push(matrix[j][i])
+    }
+    result.push(row)
+  }
+  return result
+};
